8e72d393175a6649714ef6032a467a16f753f966
Chase <105643347+spacechase03@users.noreply.github.com>
2022-08-03 10:32:54 -0500

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,17 @@
+const nav = document.querySelector('.nav');
+const navToggle = document.querySelector('.hamburger');
+
+navToggle.addEventListener('click', () => {
+    const visibility = nav.getAttribute('data-visibile')
+
+    if (visibility === "false") {
+        nav.setAttribute('data-visibile', true);
+        navToggle.setAttribute('aria-expanded', true);
+    } 
+
+    else if (visibility === "true") {
+        nav.setAttribute('data-visibile', false);
+        navToggle.setAttribute('aria-expanded', false);
+        
+    }
+});
